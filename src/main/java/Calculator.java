afb9702349c2a573bb79ef91d01cdce35682d984
NewUser <nd.dotcode@gmail.com>
2023-11-23 17:18:52 +0300

    update README.md + add Calculator methods and tests

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,35 @@
 public class Calculator {
-    public int add(int a, int b){
-        //TODO inser your realisation in method add
+    public int add(int a, int b) {
+        return a + b;
     }
-    public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+
+    public int dif(int a, int b) {
+        return a - b;
     }
-    public int div(int a, int b){
-        //TODO inser your realisation in method div
+
+    public int div(int a, int b) {
+        if (b == 0)
+            throw new ArithmeticException("Деление на 0 запрещено!");
+        return a / b;
     }
-    public int times(int a, int b){
-        //TODO inser your realisation in method times
+
+    public int times(int a, int b) {
+        return a * b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+
+    public int solver(int a, int b, char operator) {
+        switch (operator) {
+            case '+':
+                return add(a, b);
+            case '-':
+                return dif(a, b);
+            case '*':
+                return times(a, b);
+            case '/':
+                return div(a, b);
+            default:
+                throw new IllegalArgumentException("Такого оператора нет :( : " + operator);
+        }
     }
 }
+
